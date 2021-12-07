8000d417e8059e95a24f2cb2d144de465c174ab1
Simon Rieß <riess.simon@yahoo.de>
2021-12-07 10:26:46 +0100

    Autofocus

--- lib/ChatBot.jsx
+++ lib/ChatBot.jsx
@@ -781,7 +781,7 @@ class ChatBot extends Component {
             {!currentStep.hideInput && (
               <Input
                 type="textarea"
-                autoFocus
+                autoFocus={true}
                 style={inputStyle}
                 ref={this.setInputRef}
                 className="rsc-input"
